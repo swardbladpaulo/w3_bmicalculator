29214dd9aa82c673602cceba45343012d1b5ed59
swardbladpaulo <paulo@swardblads.se>
2020-10-20 16:35:51 +0200

    modified row 5 ('result') to (result), added by misstake '' and it behave like a string. Run cypress test and all green

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -2,7 +2,7 @@ const calculateBMI = () => {
   let weightInput = document.getElementById("weight")
   let heightInput = document.getElementById("height")
   let results = calculateMetricBMI(weightInput.value, heightInput.value)
-  let message = getBMIClassification('results')
+  let message = getBMIClassification(results)
   let displayResultsElement = document.getElementById('results')
   displayResultsElement.innerHTML = `<h1> Your BMI value is: ${results} and you are ${message}</h1>`
 }
