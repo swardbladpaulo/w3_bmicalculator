42b876177704f82d1a39a1ac1694f9f5a1a02f46
swardbladpaulo <paulo@swardblads.se>
2020-10-20 14:19:17 +0200

    added onClick function, modified bmiUI.js

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -1,5 +1,7 @@
 const calculateBMI = () => {
   let weightInput = document.getElementById("weight")
   let heightInput = document.getElementById("height")
-  let results = calculateMetricBMI(weightInput.nodeValue, heightInput.value)
+  let results = calculateMetricBMI(weightInput.value, heightInput.value)
+  let displayResultsElement = document.getElementById('results')
+  displayResultsElement.innerHTML = `<h1> Your BMI value is: ${results}</h1>`
 }
